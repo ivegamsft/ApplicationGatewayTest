43e87150b34879f577d3d439aa980260c65fbc5e
Jonathan Gardner <jogardn@microsoft.com>
2020-05-21 21:39:23 -0700

    Add subnet module

---
+++ env/module/subnet/main.tf
@@ -0,0 +1,12 @@
+variable "name" {}
+variable "resource_group_name" {}
+variable "virtual_network_name" {}
+variable "address_prefixes" {}
+
+resource "azurerm_subnet" "subnet" {
+  name = var.name
+
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.virtual_network_name
+  address_prefixes     = ["${var.address_prefixes}"]
+}
